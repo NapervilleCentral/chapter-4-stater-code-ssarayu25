94238279cf98b5f1f1872fc9e53f85d5c44a0ca0
Sarayu <89226972+ssarayu25@users.noreply.github.com>
2021-11-29 15:08:35 -0600

    creating student class

--- Assignments/Student.java
+++ Assignments/Student.java
@@ -8,19 +8,95 @@ public class Student
 {
    private String firstName, lastName;
    private Address homeAddress, schoolAddress;
+   public Double test1, test2, test3;
 
    //-----------------------------------------------------------------
    //  Sets up this Student object with the specified initial values.
    //-----------------------------------------------------------------
-   public Student (String first, String last, Address home,
-                   Address school)
+   public Student (String first, String last, Address home)
    {
       firstName = first;
       lastName = last;
       homeAddress = home;
-      schoolAddress = school;
+      schoolAddress = new Address(" 440 Aurora Av", "Naperville", "Illinois", 60540);
+      test1 = 0.0;
+      test2 = 0.0;
+      test3 = 0.0;
    }
-
+   
+   /**
+    * Sets the test score 
+    * @param int of test number and int of test score
+    */
+   
+   public void setTestScore (int test, double score)
+   {
+       if (test == 1)
+       {
+           test1 = score;
+        }
+       else if (test == 2)
+       {
+           test2 = score;
+        }
+       else if (test == 3)
+       {
+           test3 = score;
+        }
+        
+       
+    }
+    
+    /**
+    * gets the test score 
+    * @param int of test number
+    * @return int of test score
+    */
+   
+   public double getTestScore (int test)
+   {
+       if (test == 1)
+          return test1;
+       else if (test == 2)
+          return test2;
+       else if (test == 3)
+          return test3;
+       return 0;
+    }
+    
+    
+   /**
+    * gets the test score 
+    * @param int of test number
+    * @return int of test score
+    */
+   
+   public double average ()
+   {
+       double avg = (test1 + test2 + test3)/3;
+       return avg;
+    }
+   
+    
+   /**
+    * checks to see if two student objects have the same name
+    * @param Student object
+    * @return boolean value
+    */
+   
+   public boolean equals (Student obj)
+   {
+       if (lastName.equals(obj.lastName))
+       {
+            if (firstName.equals(obj.firstName))
+                return true;
+            else 
+                return false;
+            }
+       else
+        return false;
+    }
+    
    //-----------------------------------------------------------------
    //  Returns this Student object as a string.
    //-----------------------------------------------------------------
@@ -31,6 +107,12 @@ public class Student
       result = firstName + " " + lastName + "\n";
       result += "Home Address:\n" + homeAddress + "\n";
       result += "School Address:\n" + schoolAddress;
+      
+      result+= "test score 1: " + test1 + "\n";
+      result += "test score 2: " + test2 + "\n";
+      result += "test score 3: " + test3 + "\n";
+      
+      result += "average: " + ((test1 + test2 + test3)/3);
 
       return result;
    }
